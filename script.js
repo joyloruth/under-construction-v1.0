1d00c90ff62ce8f2fff7794ffa3d8a1dc25c53d7
joyloruth <joyloruth@gmail.com>
2025-02-04 12:29:14 -0600

    created html, links

---
+++ script.js
@@ -0,0 +1,9 @@
+const launch = (domain) =>{
+
+    // window.location.href = "https://www." + domain
+}
+
+let githubBtn = document.getElementById('github-btn')
+
+githubBtn.addEventListener('click', launch("github.com/joyloruth"))
+
