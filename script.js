a4d10f800b5af244a23a5fca35a4d29dd9f239b8
joyloruth <joyloruth@gmail.com>
2025-02-05 16:55:28 -0600

    updated avatar and added media icons

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-const launch = (domain) =>{
 
-    // window.location.href = "https://www." + domain
-}
-
-let githubBtn = document.getElementById('github-btn')
-
-githubBtn.addEventListener('click', launch("github.com/joyloruth"))
 
